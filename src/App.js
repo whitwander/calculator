259f659f4981c5376ed8ea114c94d9858d64a898
WhitWander <vcmikhaelcp@gmail.com>
2024-05-22 17:01:54 -0300

    adding operators

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useReducer } from "react";
 import "./styles.css"
 import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
 
 export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
@@ -35,20 +36,20 @@ function App() {
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <DigitButton digit="/" dispatch={dispatch} />
+      <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
       <DigitButton digit="3" dispatch={dispatch} />
-      <button>*</button>
+      <OperationButton operation="*" dispatch={dispatch} />
       <DigitButton digit="4" dispatch={dispatch} />
       <DigitButton digit="5" dispatch={dispatch} />
       <DigitButton digit="6" dispatch={dispatch} />
-      <button>+</button>
+      <OperationButton operation="+" dispatch={dispatch} />
       <DigitButton digit="7" dispatch={dispatch} />
       <DigitButton digit="8" dispatch={dispatch} />
       <DigitButton digit="9" dispatch={dispatch} />
-      <button>-</button>
-      <button>.</button>
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
     </div>
